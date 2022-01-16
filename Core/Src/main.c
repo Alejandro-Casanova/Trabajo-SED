654bf92fac9102c6ae5b7826de7d386bd9f0adfa
alejandro.casanova.martin@alumnos.upm.es <alejandro.casanova.martin@alumnos.upm.es>
2022-01-16 18:35:32 +0100

    EXTI y otras mejoras

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -24,9 +24,11 @@
 /* USER CODE BEGIN Includes */
 #include <stdbool.h>
 #include <string.h>
+/*
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+*/
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -46,45 +48,58 @@
 /* Private variables ---------------------------------------------------------*/
 I2C_HandleTypeDef hi2c1;
 
+RNG_HandleTypeDef hrng;
+
 TIM_HandleTypeDef htim7;
 
+UART_HandleTypeDef huart4;
+
 /* USER CODE BEGIN PV */
+
 // VARIABLES DISPLAY
-//Direcciónes I2C
+//Direcciones I2C y buffers
 uint8_t slave_address_write = 0x78;
 uint8_t slave_address_read = 0x78 | 0x01;
-
-bool COM_ERROR = false; //Indica error de comunicación I2C con el display
-#define alto_display 64
-#define ancho_display 128
-#define n_paginas 8
-uint8_t buffer[n_paginas * ancho_display]; //Información que se cargará en la SRAM del display
-
 uint8_t i2cTxBuf[2];
 uint8_t i2cRxBuf;
+bool COM_ERROR = false; //Indica error de comunicación I2C con el display
+bool UART_ERROR = false; //Indica error de envío UART
+
+#define alto_display 64
+#define ancho_display 128
+#define n_paginas 8 //El direccionamiento de las celdas SRAM se hace mediante páginas (8 páginas de 128 x 8 bits)
+uint8_t buffer[n_paginas * ancho_display]; //Información que se cargará en la SRAM del display
 
 //VARIABLES JUEGO
-uint8_t map_layout[ancho_display][alto_display];
-bool refresh = false;
+uint8_t map_layout[ancho_display][alto_display]; //Matriz 2D del mapa del juego, implementado de esta forma por
+												 //comodidad. Cada entero almacenará '0' o '1' para indicar bit
+												 //encendido o apagado
+bool refresh = false; //Actualizado por interrupciones de teporizador, indicará que el juego debe avanzar un
+				      //frame y que el display debe ser actualizado
 
 //Cuerpo de la serpiente
-struct modulo{
+#define LONGITUD_INICIAL 10
+#define LONGITUD_MAXIMA 99
+struct modulo{ //La serpiente estará compuesta por módulos, las frutas serán módulos individuales
 	uint8_t x;
 	uint8_t y;
 }typedef modulo;
-modulo serpiente[200];
-uint8_t s_Longitud = 10;
+modulo serpiente[LONGITUD_MAXIMA]; //Cuerpo de la serpiente
+uint8_t s_Longitud = LONGITUD_INICIAL; //Longitud actual. Este valor será enviado a la FPGA como "puntuación"
+
 //Frutas para comer por la serpiente
-#define N_FRUTAS 10
-modulo frutas[N_FRUTAS];
+#define N_FRUTAS 20 //Frutas que habrá en el mapa en todo momento
+modulo frutas[N_FRUTAS]; //Contiene todas las frutas actuales
 
 //Dirección de la serpiente
 #define s_DERECHA 'r'
 #define s_IZQUIERDA 'i'
 #define s_ARRIBA 'u'
 #define s_ABAJO 'd'
-uint8_t s_Sentido = s_DERECHA;
+uint8_t s_Sentido = s_DERECHA; //Almacena el estado de la serpiente (en qué dirección se está moviendo).
 
+//VARIABLES BOTON
+bool boton = false;
 
 /* USER CODE END PV */
 
@@ -93,8 +108,10 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_I2C1_Init(void);
 static void MX_TIM7_Init(void);
-
+static void MX_RNG_Init(void);
+static void MX_UART4_Init(void);
 /* USER CODE BEGIN PFP */
+
 //FUNCIONES OLED
 bool oled_command(uint8_t command); //Envía un comando simple al display (NO UTILIZADO)
 void oled_init(); //Inicialización Obligatoria
@@ -102,22 +119,36 @@ void alimentacion(bool enable); //Enciende o apaga el display
 void clear(bool enable); //Carga todos los valores del buffer con un mismo valor
 void display(); //Carga el buffer en la SRAM del display
 void dibuja_pixel(uint8_t x, uint8_t y, bool borra); //Dibuja/Borra un sólo pixel del buffer
-void flash_screen(); //Hace parpadear la pantalla (Todo ON/Valores SRAM)
+//void flash_screen(); //Hace parpadear la pantalla (Todo ON/Valores SRAM)
+void flashScreen(); //Hace parpadear la pantalla (invertida/normal) 4 veces
+void setContrast(uint8_t value); //Define el contraste del display, "value" entre 0 y 255
 
 //FUNCIONES JUEGO
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* htim); //Callback encargada de avanzar el juego y refrescar la pantalla cada 50ms
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* htim); //Callback encargada de avanzar el juego y
+															 //refrescar la pantalla cada 50ms (20Hz)
 void clearLayout(); //Limpia el layout
-void setLayout(); //Dibuja las paredes y BORRA LO DEMÁS
+void setLayout(); //Dibuja las paredes y BORRA LO DEM�?S
 void init_serpiente(); //Inicializa la serpiente en posicion inicial
 void init_frutas(); //Inicializa las posiciones iniciales de las frutas (debe ejecutarse después de setSerpiente)
+void lanzaFruta(); //Posiciona una fruta en un lugar aleatorio
 void setSerpiente(); //Dibuja la serpiente en el layout
 void setFrutas(); //Dibuja las frutas en el layout
 void transferMapToBuffer(); //Carga el mapa en el buffer que luego se envía al display
-bool giraSerpiente(uint8_t indicacion); //Cambia la dirección de la serpiente (Hay macros con cada dirección) Devuelve false si no se pudo ejecutar el giro
+bool giraSerpiente(uint8_t indicacion); //Cambia la dirección de la serpiente (Hay macros con cada dirección)
+										//Devuelve false si no se pudo ejecutar el giro
 void giraDerecha(); //Gira la serpiente 90º a la derecha
+void giraIzquierda(); //Gira la serpiente 90º a la izquierda
 void avanzaSerpiente(); //Hace avanzar la serpiente una casilla (en el layout)
-int colision(uint8_t x, uint8_t y); //Devuelve 1 si la colisión es con una fruta, 2 con pared, -1 si es la propia serpiente y 0 si no hay colisión
+int colision(uint8_t x, uint8_t y); //Devuelve 1 si la colisión es con una fruta, 2 con pared,
+									//-1 si es la propia serpiente y 0 si no hay colisión
 void gameOver();
+void initGame();
+
+//FUNCIONES PULSADOR
+bool debounce(volatile bool* boton);
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin);
+
+
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -155,19 +186,22 @@ int main(void)
   MX_GPIO_Init();
   MX_I2C1_Init();
   MX_TIM7_Init();
+  MX_RNG_Init();
+  MX_UART4_Init();
   /* USER CODE BEGIN 2 */
 
-  srand((unsigned)time(0)); //Inicializa generador de números aleatorios
   oled_init(); //INICIALIZACIÓN OBLIGATORIA
-  init_serpiente();
+//  init_serpiente();
+//
+//  //clearLayout(); //setLayout() lo hace
+//  setLayout();
+//  setSerpiente();
+//  init_frutas();
+//  setFrutas();
+//  transferMapToBuffer();
+//  display();
 
-  //clearLayout(); //setLayout() lo hace
-  setLayout();
-  setSerpiente();
-  init_frutas();
-  setFrutas();
-  transferMapToBuffer();
-  display();
+  initGame();
 
   HAL_TIM_Base_Start_IT(&htim7);
   /* USER CODE END 2 */
@@ -218,6 +252,9 @@ int main(void)
 //		  HAL_Delay(50);
 //	  }
 //	  giraSerpiente(s_DERECHA);
+
+
+	  /*
 	  while(a < 100){
 		  if(refresh){
 			  avanzaSerpiente();
@@ -230,6 +267,23 @@ int main(void)
 			  a++;
 			  refresh = false;
 			  if((a % 10) == 0)
+				  giraDerecha();
+		  }
+	  }
+	  */
+
+	  uint32_t count = HAL_GetTick();
+	  while(HAL_GetTick() - count < 30000){
+		  if(refresh){
+			  avanzaSerpiente();
+			  setLayout();
+			  setSerpiente();
+			  setFrutas();
+			  transferMapToBuffer();
+			  display();
+
+			  refresh = false;
+			  if(debounce(&boton))
 				  giraDerecha();
 		  }
 	  }
@@ -258,7 +312,12 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 192;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -313,6 +372,32 @@ static void MX_I2C1_Init(void)
 }
 
 /**
+  * @brief RNG Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_RNG_Init(void)
+{
+
+  /* USER CODE BEGIN RNG_Init 0 */
+
+  /* USER CODE END RNG_Init 0 */
+
+  /* USER CODE BEGIN RNG_Init 1 */
+
+  /* USER CODE END RNG_Init 1 */
+  hrng.Instance = RNG;
+  if (HAL_RNG_Init(&hrng) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN RNG_Init 2 */
+
+  /* USER CODE END RNG_Init 2 */
+
+}
+
+/**
   * @brief TIM7 Initialization Function
   * @param None
   * @retval None
@@ -351,15 +436,67 @@ static void MX_TIM7_Init(void)
 }
 
 /**
+  * @brief UART4 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_UART4_Init(void)
+{
+
+  /* USER CODE BEGIN UART4_Init 0 */
+
+  /* USER CODE END UART4_Init 0 */
+
+  /* USER CODE BEGIN UART4_Init 1 */
+
+  /* USER CODE END UART4_Init 1 */
+  huart4.Instance = UART4;
+  huart4.Init.BaudRate = 115200;
+  huart4.Init.WordLength = UART_WORDLENGTH_8B;
+  huart4.Init.StopBits = UART_STOPBITS_1;
+  huart4.Init.Parity = UART_PARITY_NONE;
+  huart4.Init.Mode = UART_MODE_TX;
+  huart4.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart4.Init.OverSampling = UART_OVERSAMPLING_8;
+  if (HAL_HalfDuplex_Init(&huart4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN UART4_Init 2 */
+
+  /* USER CODE END UART4_Init 2 */
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
   */
 static void MX_GPIO_Init(void)
 {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pins : PA0 PA1 PA2 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI0_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI0_IRQn);
+
+  HAL_NVIC_SetPriority(EXTI1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI1_IRQn);
+
+  HAL_NVIC_SetPriority(EXTI2_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI2_IRQn);
 
 }
 
@@ -490,7 +627,7 @@ void dibuja_pixel(uint8_t x, uint8_t y, bool borra){
 	else
 		buffer[x + (y / 8) * ancho_display] &= ~(1 << (y & 7)); // clear bit
 }
-void flash_screen(){
+/*void flash_screen(){
 	i2cTxBuf[0] = 0x00; //Command
 	i2cTxBuf[1] = 0xA4;
 	if(HAL_I2C_Master_Transmit(&hi2c1, slave_address_write, i2cTxBuf, 2, HAL_MAX_DELAY) != HAL_OK){
@@ -500,6 +637,41 @@ void flash_screen(){
 	i2cTxBuf[1] = 0xA5;
 	HAL_I2C_Master_Transmit(&hi2c1, slave_address_write, i2cTxBuf, 2, HAL_MAX_DELAY);
 	HAL_Delay(500);
+}*/
+
+void flashScreen(){
+	uint8_t flash_on[] = {
+		0x00, //Command
+		0xA7 //Inverse Display
+	};
+	uint8_t flash_off[] = {
+		0x00, //Command
+		0xA6 //Normal Display
+	};
+
+	for(int i = 0; i < 4; i++){
+		if (HAL_I2C_Master_Transmit(&hi2c1, slave_address_write, flash_on, sizeof(flash_on), HAL_MAX_DELAY) != HAL_OK){
+				COM_ERROR = true;
+		}
+		HAL_Delay(250);
+		if (HAL_I2C_Master_Transmit(&hi2c1, slave_address_write, flash_off, sizeof(flash_off), HAL_MAX_DELAY) != HAL_OK){
+				COM_ERROR = true;
+		}
+		HAL_Delay(250);
+	}
+}
+
+void setContrast(uint8_t value){
+	uint8_t contrast_config[] = {
+			0x00, //Command
+			0x81, //Contrast Control (0x81 en ssd1306)
+			value
+	};
+
+	if (HAL_I2C_Master_Transmit(&hi2c1, slave_address_write, contrast_config, sizeof(contrast_config), HAL_MAX_DELAY) != HAL_OK){
+			COM_ERROR = true;
+	}
+
 }
 
 //FUNCIONES JUEGO
@@ -529,18 +701,35 @@ void init_serpiente(){
 }
 
 void init_frutas(){
-	uint8_t x, y;
-	for(int i = 0; i < N_FRUTAS;){
-		x = (rand() % (ancho_display - 20)) + 10;
-		y = (rand() % (alto_display - 20)) + 10;
+	//uint32_t x, y;
+	for(int i = 0; i < N_FRUTAS; i++){
+		/*HAL_RNG_GenerateRandomNumber(&hrng, &x);
+		HAL_RNG_GenerateRandomNumber(&hrng, &y);
+		x = (x % (ancho_display - 20)) + 10;
+		y = (y % (alto_display - 20)) + 10;
 		if(map_layout[x][y] == 0){
 			frutas[i].x = x;
 			frutas[i].y = y;
 			i++;
-		}
+		}*/
+
+		lanzaFruta(&frutas[i]);
 	}
 }
 
+void lanzaFruta(modulo* fruta){ //Posiciona una fruta en un lugar aleatorio
+	uint32_t x, y;
+	//for(int i = 0; i < N_FRUTAS;){
+	do{
+		HAL_RNG_GenerateRandomNumber(&hrng, &x);
+		HAL_RNG_GenerateRandomNumber(&hrng, &y);
+		x = (x % (ancho_display - 20)) + 10;
+		y = (y % (alto_display - 20)) + 10;
+	}while(map_layout[x][y] != 0);
+
+	fruta->x = x;
+	fruta->y = y;
+}
 void setSerpiente(){
 	for(int i = 0; i < s_Longitud; i++){
 		map_layout[serpiente[i].x][serpiente[i].y] = 1;
@@ -590,6 +779,17 @@ void giraDerecha(){
 		s_Sentido = s_DERECHA;
 }
 
+void giraIzquierda(){
+	if(s_Sentido == s_DERECHA)
+		s_Sentido = s_ARRIBA;
+	else if(s_Sentido == s_ABAJO)
+		s_Sentido = s_DERECHA;
+	else if(s_Sentido == s_IZQUIERDA)
+		s_Sentido = s_ABAJO;
+	else
+		s_Sentido = s_IZQUIERDA;
+}
+
 void avanzaSerpiente(){
 
 	//No comprueba si el sentido es apropiado (dicha comprobación se hace en la función giraSerpiente()
@@ -610,17 +810,19 @@ void avanzaSerpiente(){
 	}
 
 	switch(colision(serpiente[0].x, serpiente[0].y)){
-	case -1: //Serpiente
-	case 2: //Pared
-		gameOver();
-		break;
-	case 0: //Sin Colisión
-		break;
-	case 1: //Fruta (serpiente crece 1)
-		s_Longitud++;
-		serpiente[s_Longitud - 1].x = serpiente[s_Longitud - 2].x - (serpiente[s_Longitud - 3].x - serpiente[s_Longitud - 2].x);
-		serpiente[s_Longitud - 1].y = serpiente[s_Longitud - 2].y - (serpiente[s_Longitud - 3].y - serpiente[s_Longitud - 2].y);
-		break;
+		case -1: //Serpiente
+		case 2: //Pared
+			gameOver();
+			break;
+		case 0: //Sin Colisión
+			break;
+		case 1: //Fruta (serpiente crece 1)
+			s_Longitud++;
+			serpiente[s_Longitud - 1].x = serpiente[s_Longitud - 2].x - (serpiente[s_Longitud - 3].x - serpiente[s_Longitud - 2].x);
+			serpiente[s_Longitud - 1].y = serpiente[s_Longitud - 2].y - (serpiente[s_Longitud - 3].y - serpiente[s_Longitud - 2].y);
+			if(HAL_UART_Transmit(&huart4, &s_Longitud, sizeof(s_Longitud), HAL_MAX_DELAY) != HAL_OK)
+				UART_ERROR = true;
+			break;
 	}
 
 	/*
@@ -638,24 +840,70 @@ void avanzaSerpiente(){
 
 int colision(uint8_t x, uint8_t y){
 
-	if(x == ancho_display - 1 || x == 0 || y == alto_display - 1 || y == 0) //Colisión con pared
-		return 2;
+	if(map_layout[x][y] == 1){ //Colisión con ente desconocido
+		if(x == ancho_display - 1 || x == 0 || y == alto_display - 1 || y == 0) //Colisión con pared
+			return 2;
 
-	for(int i = 0; i < N_FRUTAS; i++){
-		if(frutas[i].x == x && frutas[i].y == y) //Comprueba colisión con fruta
-			return 1;
-	}
+		for(int i = 0; i < N_FRUTAS; i++){
+			if(frutas[i].x == x && frutas[i].y == y){ //Comprueba colisión con fruta
+				lanzaFruta(&frutas[i]); //Repone la fruta en otro sitio
+				return 1;
+			}
+		}
 
-	for(int i = 0; i < s_Longitud; i++){ //Colisión con serpiente
-		if(serpiente[i].x == x && serpiente[i].y == y)
-			return -1;
+		for(int i = 0; i < s_Longitud; i++){ //Colisión con serpiente
+			if(serpiente[i].x == x && serpiente[i].y == y)
+				return -1;
+		}
 	}
 
 	return 0;
 }
 
+bool debounce(volatile bool* boton){
+	static int counter = 0;
+	//static bool first_time = true;
+
+	if(*boton == true){
+		if((HAL_GetTick() - counter) > 100 && HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0)){ //No es rebote
+			counter = HAL_GetTick();
+			*boton = false;
+			return true;
+		}else{ //Es Rebote
+			counter = HAL_GetTick();
+			*boton = false;
+			return false;
+		}
+	}else{
+		return false;
+	}
+
+}
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
+	if (GPIO_Pin == GPIO_PIN_0){
+			boton = true;
+	}else if (GPIO_Pin == GPIO_PIN_1)
+		giraDerecha();
+	else if (GPIO_Pin == GPIO_PIN_2)
+		giraIzquierda();
+}
+
 void gameOver(){
-	return;
+	flashScreen();
+	initGame();
+}
+
+void initGame(){
+	s_Longitud = LONGITUD_INICIAL;
+	init_serpiente();
+	//clearLayout(); //setLayout() lo hace
+	setLayout();
+	setSerpiente();
+	init_frutas();
+	setFrutas();
+	transferMapToBuffer();
+	display();
 }
 
 
--- Core/Src/stm32f4xx_hal_msp.c
+++ Core/Src/stm32f4xx_hal_msp.c
@@ -145,6 +145,50 @@ void HAL_I2C_MspDeInit(I2C_HandleTypeDef* hi2c)
 }
 
 /**
+* @brief RNG MSP Initialization
+* This function configures the hardware resources used in this example
+* @param hrng: RNG handle pointer
+* @retval None
+*/
+void HAL_RNG_MspInit(RNG_HandleTypeDef* hrng)
+{
+  if(hrng->Instance==RNG)
+  {
+  /* USER CODE BEGIN RNG_MspInit 0 */
+
+  /* USER CODE END RNG_MspInit 0 */
+    /* Peripheral clock enable */
+    __HAL_RCC_RNG_CLK_ENABLE();
+  /* USER CODE BEGIN RNG_MspInit 1 */
+
+  /* USER CODE END RNG_MspInit 1 */
+  }
+
+}
+
+/**
+* @brief RNG MSP De-Initialization
+* This function freeze the hardware resources used in this example
+* @param hrng: RNG handle pointer
+* @retval None
+*/
+void HAL_RNG_MspDeInit(RNG_HandleTypeDef* hrng)
+{
+  if(hrng->Instance==RNG)
+  {
+  /* USER CODE BEGIN RNG_MspDeInit 0 */
+
+  /* USER CODE END RNG_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_RNG_CLK_DISABLE();
+  /* USER CODE BEGIN RNG_MspDeInit 1 */
+
+  /* USER CODE END RNG_MspDeInit 1 */
+  }
+
+}
+
+/**
 * @brief TIM_Base MSP Initialization
 * This function configures the hardware resources used in this example
 * @param htim_base: TIM_Base handle pointer
@@ -190,6 +234,69 @@ void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* htim_base)
   /* USER CODE BEGIN TIM7_MspDeInit 1 */
 
   /* USER CODE END TIM7_MspDeInit 1 */
+  }
+
+}
+
+/**
+* @brief UART MSP Initialization
+* This function configures the hardware resources used in this example
+* @param huart: UART handle pointer
+* @retval None
+*/
+void HAL_UART_MspInit(UART_HandleTypeDef* huart)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(huart->Instance==UART4)
+  {
+  /* USER CODE BEGIN UART4_MspInit 0 */
+
+  /* USER CODE END UART4_MspInit 0 */
+    /* Peripheral clock enable */
+    __HAL_RCC_UART4_CLK_ENABLE();
+
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    /**UART4 GPIO Configuration
+    PC10     ------> UART4_TX
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_10;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF8_UART4;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN UART4_MspInit 1 */
+
+  /* USER CODE END UART4_MspInit 1 */
+  }
+
+}
+
+/**
+* @brief UART MSP De-Initialization
+* This function freeze the hardware resources used in this example
+* @param huart: UART handle pointer
+* @retval None
+*/
+void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
+{
+  if(huart->Instance==UART4)
+  {
+  /* USER CODE BEGIN UART4_MspDeInit 0 */
+
+  /* USER CODE END UART4_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_UART4_CLK_DISABLE();
+
+    /**UART4 GPIO Configuration
+    PC10     ------> UART4_TX
+    */
+    HAL_GPIO_DeInit(GPIOC, GPIO_PIN_10);
+
+  /* USER CODE BEGIN UART4_MspDeInit 1 */
+
+  /* USER CODE END UART4_MspDeInit 1 */
   }
 
 }
